91d7072b4ef0dcf5a839233d2a6b64f28bc42d4b
github username <github email address>
2019-12-19 13:49:39 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -6,4 +6,9 @@ ActiveRecord::Base.establish_connection(
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
 
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+ActiveRecord::Base.connection
+
+
 require_all 'app'
